70769e5f77adfb3ff3c2237cb15210980e1cd8c5
Deepak kumar <structlooper@gmail.com>
2021-12-24 13:27:00 +0530

    service page added

--- src/screens/auth/BookAppointment/SpecialistServices/ServiceList.js
+++ src/screens/auth/BookAppointment/SpecialistServices/ServiceList.js
@@ -1,14 +1,38 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { FlatList, Image, ScrollView, Text, TouchableOpacity, View } from "react-native";
 import { colors } from "../../../../theme/colors";
 import statusBar from "../../../../componenets/molecules/statusBar";
 import HeaderVerifyIdentity from "../../../../componenets/molecules/HeaderVerifyIdentity";
-import { widthPercentageToDP } from "react-native-responsive-screen";
+import {  widthPercentageToDP } from "react-native-responsive-screen";
 import SearchInput from "../../../../componenets/atoms/SearchInput";
+import { style } from "../../../../componenets/organisms/style";
 
 const ServiceList = ({ navigation }) => {
   const [exitModal,SetExitModal] = useState(false)
   const [searched,SetSearched] = useState('');
+
+  const ServiceBadge = ({icon,text}) => {
+    return (
+      <TouchableOpacity style={{
+        width:130,
+        height:130,
+        borderRadius:10,
+        backgroundColor:colors.light,
+        elevation:5,
+        alignItems:'center',
+        justifyContent:'center',
+        margin:'2%'
+      }} onPress={()=> {
+        console.log(text)
+      }}>
+        <Image source={icon} style={{ height:90,width:90,resizeMode:'contain' }} />
+        <Text style={style.commonTitle}>
+          {text}
+        </Text>
+      </TouchableOpacity>
+    )
+  }
+  const ServiceListRender = ({ item }) => <ServiceBadge text={item.text} icon={item.icon} />
   return (
     <View style={{ flex:1,backgroundColor:colors.light }} >
       {statusBar(colors.light)}
@@ -24,13 +48,60 @@ const ServiceList = ({ navigation }) => {
       />
       <View style={{ flex:.8,paddingHorizontal:widthPercentageToDP(5)}}>
         <View>
-          <SearchInput Search={searched} color={colors.pink} SetSearch={SetSearched} placeHolder={'Search specialist..'} />
+          <SearchInput Search={searched} color={colors.pink} SetSearch={SetSearched} placeHolder={'Search speciality ...'} />
         </View>
+        <ScrollView>
+
+        <FlatList
+          data={[
+            {
+              text:'Cardiology',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/68.png')
+            },
+            {
+              text:'Orthopedic',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/69.png')
+            },
+            {
+              text:'Dermatology',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/70.png')
+            },
+            {
+              text:'OBGYN',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/71.png')
+            },
+            {
+              text:'Otology',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/73.png')
+            },
+            {
+              text:'Pulmonology',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/74.png')
+            },
+            {
+              text:'Nephrology',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/75.png')
+            },
+            {
+              text:'Radiology',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/67.png')
+            },
+            {
+              text:'Hematology',
+              icon:require('../../../../assets/images/SpecialistServiceIcons/72.png')
+            },
+          ]}
+          numColumns={3}
+          renderItem={ServiceListRender}
+          keyExtractor={item => item.text}
+
+        />
+        </ScrollView>
+
       </View>
 
-      <View style={{ flex:.2 }}>
+      <View style={{ flex:.2 }} />
 
-      </View>
     </View>
   );
 };
